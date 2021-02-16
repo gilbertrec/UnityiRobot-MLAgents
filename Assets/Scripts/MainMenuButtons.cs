2ce169468e4be4d138ab1a533e52a0d15a7c7e5e
gilbertrec <gilbertrecupito@gmail.com>
2021-02-16 20:04:46 +0100

    Added Main Menu Scene

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenuButtons : MonoBehaviour
+{
+
+
+    public Button b1 ;
+    public Button b2 ;
+    public Button b3 ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        b1.onClick.AddListener(delegate{LoadScene1();});
+        
+        b2.onClick.AddListener(delegate{LoadScene2();});
+        
+        b3.onClick.AddListener(delegate{Debug.Log("aaaa");LoadScene3();});
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene1(){
+        Debug.Log("aaaaaaa");
+        SceneManager.LoadScene("Scene1_v2");
+    }
+    
+    public void LoadScene2(){
+        SceneManager.LoadScene("Scene2");
+    }
+    
+    public void LoadScene3(){
+        SceneManager.LoadScene("Scene3_v2");
+    }
+}
